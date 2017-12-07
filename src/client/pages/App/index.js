74611ef5dc82ea482d3e4f03f2b3e800d4211707
Lcharvol <charvolin0@google.com>
2017-12-07 20:04:43 +0100

    Init client

---
+++ src/client/pages/App/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { AppContainer } from './style';
+
+const App = () => (
+  <AppContainer>
+  </AppContainer>
+);
+
+export default App;
---
+++ src/client/pages/App/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.div`
+  display:flex;
+  width:100%;
+  height:100vh;
+  background-color: rgb(39,40,34);
+`;
