86e727fc85b5ad590b629a40cad4a2594e9c247a
Lcharvol <charvolin0@gmail.com>
2018-01-06 17:13:26 +0100

    Infinty Scrolling is now working

---
+++ src/client/components/GoTopButton/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { GoTopButtonContainer } from './styles';
+
+const GoTopButton = () => (
+  <GoTopButtonContainer>
+  </GoTopButtonContainer>
+);
+
+export default GoTopButton;
--- src/client/pages/Movies/index.js
+++ src/client/pages/Movies/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import { array } from 'prop-types';
+import {
+  array,
+  func,
+  number,
+} from 'prop-types';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { map } from 'lodash';
+import VisibilitySensor from 'react-visibility-sensor';
 
-import { getMovies } from '../../selectors/movies';
+import {
+  getMovies,
+  getMoviesCount,
+} from '../../selectors/movies';
 import {
   MoviesContainer,
   MoviePreviewContainer,
@@ -15,8 +24,23 @@ import SearchBar from '../../components/SearchBar';
 import Spinner from '../../components/Spinner';
 import SlideSelect from '../../components/SlideSelect';
 import CheckBoxSelect from '../../components/CheckBoxSelect';
+import { reqMovies } from '../../request';
+import { addMovies } from '../../actions/movies';
 
-const Movies = ({ movies }) => (
+const onChange = (isVisible, addMovies, moviesCount) => {
+  if (isVisible) {
+    reqMovies(20, moviesCount)
+      .then(movies => addMovies(movies))
+      .catch(console.warn('Warning: Cant Load Movies!'));
+    console.log('Le Spinner et visible');
+  }
+};
+
+const Movies = ({
+  movies,
+  moviesCount,
+  addMovies,
+}) => (
   <MoviesContainer>
     <ParamsContainer>
       <SearchBar />
@@ -31,16 +55,27 @@ const Movies = ({ movies }) => (
         {map(movies, movie => <MoviePreview key={movie.imdbId} movie={movie} />)}
       </MoviePreviewContainer>
     )}
-    {movies.length <= 0 && <Spinner />}
+    {!movies.length && <Spinner />}
+    {movies.length > 0 &&
+      <VisibilitySensor onChange={isVisible => onChange(isVisible, addMovies, moviesCount)}>
+        <Spinner />
+      </VisibilitySensor>
+    }
   </MoviesContainer>
 );
 
 Movies.propTypes = {
   movies: array.isRequired,
+  moviesCount: number.isRequired,
+  addMovies: func.isRequired,
 };
+
+const actions = { addMovies };
+const mapDispatchToProps = dispatch => bindActionCreators(actions, dispatch);
 
 const mapStateToProps = state => ({
   movies: getMovies(state),
+  moviesCount: getMoviesCount(state),
 });
 
-export default connect(mapStateToProps)(Movies);
+export default connect(mapStateToProps, mapDispatchToProps)(Movies);
