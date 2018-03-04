3dbc234bb21417e040c672e7d4574308a80fabcc
abarriel <abarriel@student.42.fr>
2018-03-04 19:49:27 +0100

    scrapper almost done

--- src/server/api/service/imdb.ts
+++ src/server/api/service/imdb.ts
@@ -3,33 +3,18 @@ import * as express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 import middlewaresBinding from '../middleware';
+import * as jsonframe from 'jsonframe-cheerio';
 
-let d = {
-    "title": 'Nan',
-    "year": 'Nan',
-    "plot": 'Nan',
-    "plotKeywords": 'Nan',
-    "genre": 'Nan',
-    "country": 'Nan',
-    "language": 'Nan',
-    "released": 'Nan',
-    "runtime": 'Nan',
-    "writer": 'Nan',
-    "actors": 'Nan',
-    "poster": 'Nan',
-    "imdbRating": 'Nan',
-    "imdbVotes": 'Nan',
-    "imdbID": 'Nan',
-    "type": 'Nan',
+const SHOW = 'tv_show';
+const EPISODE = 'episode';
+const MOVIE = 'movie';
+const YEARSTV = 'years';
+const NEED_INT_CAST = ['year', 'imdbRating', 'popularity', 'metascore', 'seasons', 'episode'];
+const KEY_MOVIE = []
+// title, imdbShowId, showName, season, episode, year, years, released, genre, director, writers, production, actors, resume, runtime, country, imdbRating, popularity, metascore, poster, type, seasons,
 
-    "director": 'Nan',
-    "metascore": 'Nan',
-
-    "popularity": 'Nan',
-    "creators": 'Nan',
-    "totalSeasons": 'Nan',
-}
-
+const KEY_SHOW = []
+const KEY_EPISODE = []
 class imdbId {
   name = 'imdb';
 
@@ -37,45 +22,50 @@ class imdbId {
     const { imdbId } = req.params
     const { data } = await axios.get(`http://www.imdb.com/title/${imdbId}`);
     const $ = cheerio.load(data);
-    // const { dataPlotKeywords } =
+    jsonframe($);
 
-    // d.title = $('.title_wrapper h1:first-child').text(); // contain name and year
+    const frame = {
+      title: '.title_wrapper h1:first-child || .+?(?=\\([0-9]{4}\\)) | trim',
+      imdbShowId: '.titleParent a @ href || title\\/(.+?)(?=\\?)',
+      showName: '.titleParent a @ title',
+      season: '.button_panel.navigation_panel div:first-child .bp_heading:has(.ghost) | nb || .+?(?= )',
+      episode: '.button_panel.navigation_panel div:first-child .bp_heading:has(.ghost) | nb || \\d{1,}$',
+      year: 'a[href*="releaseinfo?ref_=tt_ov_inf"] meta[itemprop=datePublished] @ content || [0-9]{4}',
+      years: ['.seasons-and-year-nav a[href*="tt_eps_yr_"]'],
+      released: 'a[href*="releaseinfo?ref_=tt_ov_inf"] meta[itemprop=datePublished] @ content',
+      genre: ['span[itemprop=genre]'],
+      director: '.plot_summary_wrapper a[href*="ov_dr"] span[itemprop=name]',
+      writers: ['.credit_summary_item span[itemprop=creator] a'],
+      production: ['#titleDetails span[itemprop=creator]'],
+      actors: ['.cast_list span[itemprop=name]'],
+      resume: '.article [itemprop=description] || .+?(?=Written by) | trim',
+      runtime: '.title_wrapper time[itemprop=duration] @ datetime',
+      country: 'a[href*="primary_language"]:first-of-type',
+      imdbRating: '.ratingValue span[itemprop=ratingValue]',
+      popularity: 'div[class*="popularityTrend"] ~ div | nb || .+?(?= )',
+      metascore: '.titleReviewBarItem a[href*="ov_rt"] span',
+      poster: 'link[rel="image_src"] @ href',
+      type: 'meta[property="og:type"] @ content || \\.(.*)',
+      seasons: '.seasons-and-year-nav a[href*="season"]',
+    };
 
-    // SHARABLE
-    d.title = $('.originalTitle').text() .split('(original title')[0].trim()
-    d.plot = $('[itemprop=description]').text().replace(/\s\s+|Written by\n.*/gi, ' ').trim();
-    d.genre = $('span[itemprop=genre]').map((i,el) => $(el).text()).get(); // loooook i was tired haha
-    d.country = $('#titleDetails a[href*="country_of_origin"]').text();
-    d.language = $('#titleDetails a[href*="primary_language"]:first-of-type').text();
-    // d.released = $('#titleDetails div:nth-of-type(4)').text();
-    d.released = $('meta[itemprop=datePublished]').attr("content");
-    d.runtime = $('.title_wrapper time[itemprop=duration]').attr('datetime')
-    d.writer =  $('.plot_summary_wrapper a[href*="ov_wr"] span[itemprop=name]').map((i,el) => $(el).text()).get();
-    d.actors =  $('.cast_list span[itemprop=name]').map((i,el) => $(el).text()).get();
-    d.poster = $('meta[property="og:image"]').attr('content')
-    d.imdbRating = $('.ratingValue span[itemprop=ratingValue]').text()
-    d.imdbID = imdbId;
-    d.type =  $('meta[property="og:type"]').attr('content').split('.')[1];
-    d.imdbVotes = $('span[itemprop=ratingCount]').text();
-
-    // movie
-    d.director = $('.plot_summary_wrapper a[href*="ov_dr"] span[itemprop=name]').map((i,el) => $(el).text()).get().join(', ');
-    d.metascore = $('.titleReviewBarItem a[href*="ov_rt"] span').text()
-    d.year = $('a[href*="releaseinfo?ref_=tt_ov_inf"]').text();
-
-    // tvshow
-    d.creators = d.writer;
-  d.popularity = $('div[class*="popularityTrend"] ~ div').text().match(/[0-9].*/)[0];
-    d.totalSeasons = $('.seasons-and-year-nav a[href*="season"]').contents().length;
-    // sharable
-    d.plotKeywords = $('[itemprop=keywords] nobr a').attr('href');
-    const { data: dataPlotKeywords } =  await axios.get(`http://www.imdb.com${d.plotKeywords}`);
-    const $$ = cheerio.load(dataPlotKeywords);
-    d.plotKeywords = $$('table .sodatext a').map((i,el) => $(el).text()).get();
-
-    console.log(d);
-    // res.json(comments);
-    res.json({ o: 'o' });
+    const _infos = $('html').scrape(frame);
+    if (_infos.type !== 'tv_show')
+    delete _infos.years;
+    const infos = { imdbId, ..._infos };
+    const infosCast = _.reduce(infos, (acc: {}, value: any, key: string) => {
+       if (NEED_INT_CAST.includes(key))
+          return { ...acc, [key]: value * 1};
+      if (key === 'years')
+          return { ...acc, [key]: value.map(Number)};
+      return { ...acc, [key]: value };
+    } , {});
+    // const infosCast = _.map(infos, (v:any, k:any) => {
+    //   if (allInteger.includes(k))
+    //       return { v: k * 1 };
+    //   return { k : k };
+    // })
+    res.json(infosCast);
   }
 
 };
